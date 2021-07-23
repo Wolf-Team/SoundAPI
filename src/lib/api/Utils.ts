987e8d184fff630e38a94debbc6e3087d1df9b47
80LK <80lkru@gmail.com>
2021-07-23 08:19:26 +0300

    add Updatable abstract class

--- src/lib/api/Utils.ts
+++ src/lib/api/Utils.ts
@@ -6,4 +6,26 @@ namespace SoundAPI.Utils {
     export function getVolume(): Volume {
         return { left: 1, right: 1 };
     }
+
+    export abstract class Updatable {
+        protected remove: boolean = false;
+        protected abstract update(): void;
+
+        public getUpdatable(): globalThis.Updatable {
+            const _this = this;
+            return {
+                update: () => {
+                    _this.update();
+                    this.remove = _this.remove;
+                }
+            }
+        }
+
+        public addServerUpdatable() {
+            IC.Updatable.addUpdatable(this.getUpdatable())
+        }
+        public addClientUpdatable() {
+            IC.Updatable.addLocalUpdatable(this.getUpdatable())
+        }
+    }
 }
