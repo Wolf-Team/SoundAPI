981049f293f4551d9a47266ca802edf1b18d4c65
80LK <80lkru@gmail.com>
2021-07-31 19:52:42 +0300

    rewrite Timer

--- src/lib/api/Utils/Timer.ts
+++ src/lib/api/Utils/Timer.ts
@@ -3,7 +3,8 @@ namespace Utils {
     type Thread = java.lang.Thread;
     const Runnable = java.lang.Runnable;
 
-    export interface TimerHundler {
+    export interface TimerVoid { (): void }
+    export interface TimerHandler {
         startTimer?: () => void,
         endTimer: () => void
     }
@@ -13,30 +14,52 @@ namespace Utils {
 
 
     export class Timer {
-        protected _thread: Thread;
-        protected _loop: number = 1;
+        private _thread: Thread;
+        private _loop: number = 1;
+        private _time: number = 0;
+        private _run: boolean = false;
+        private _handler: TimerHandler;
 
-        constructor(hundler: TimerHundler);
-        constructor(protected _hundler: TimerHundler) { }
 
-        public start(time: number, loop: number = TimerLoop.NONE) {
-            this._loop = loop == TimerLoop.INFINITE ? loop : loop + 1;
+        constructor(hundler: TimerHandler);
+        constructor(hundler: TimerVoid);
+        constructor(hundler: TimerHandler | TimerVoid) {
+            if (typeof hundler == "function")
+                hundler = { endTimer: hundler };
+
+            this._handler = hundler;
+
             this._thread = new Thread(new Runnable({
                 run: () => {
-                    while (this._loop--) {
-                        if (this._hundler.startTimer)
-                            this._hundler.startTimer();
+                    let time = Debug.sysTime();
 
-                        Thread.sleep(time);
-                        this._hundler.endTimer();
+                    while (this._run && (this._loop == TimerLoop.INFINITE || this._loop--)) {
+                        if (this._handler.startTimer)
+                            this._handler.startTimer();
+
+                        while (this._run) {
+                            if (Debug.sysTime() - time < this._time)
+                                continue;
+
+
+                            this._handler.endTimer();
+                        }
+                        if (this._loop == 0)
+                            this._run = false;
                     }
                 }
             }));
+        }
+
+        public start(time: number, loop: number = TimerLoop.NONE) {
+            this._loop = loop == TimerLoop.INFINITE ? loop : loop + 1;
+            this._run = true;
+            this._time = time;
             this._thread.start();
         }
         public stop() {
             this._loop = 0;
-            this._thread.interrupt();
+            this._run = false;
         }
     }
 }
