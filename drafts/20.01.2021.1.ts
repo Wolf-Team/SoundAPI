c35a14bcd1980a488f6167d59544a8f834842699
80LK <80LK@users.noreply.github.com>
2022-01-21 19:59:04 +0300

    draft 20.01.2021.1

---
+++ drafts/20.01.2021.1.ts
@@ -0,0 +1,13 @@
+/** SoundPool */
+const pool = SoundPool.getPool("poolName");
+pool.registerSound("uid", "path"); // Register sound for multiplayer
+pool.playSound("path");// -> play sound from path
+pool.playSoundFromUId("uid");// -> play sound from UID
+pool.playSoundForAll("uid");// -> play sound and send Network Packet
+
+/* MediaPlayer */
+const player = MediaPlyer.get("uid"); // UID - used for multiplayer connect
+player.registerSound("uid", "path"); // Register sound for multiplayer
+player.playSound("path");// -> play sound from path
+player.playSoundFromUId("uid");// -> play sound from UID
+player.playSoundForAll("uid");// -> play sound and send Network Packet
